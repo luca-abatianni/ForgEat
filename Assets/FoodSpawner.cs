df126772cabf480fa57a152272d7cc07b46afa8b
maciejlampart <maciejlampart@live.com>
2024-04-05 18:02:10 +0200

    Added food and trash prefabs. Started implementing food spawner

--- Assets/FoodSpawner.cs
+++ Assets/FoodSpawner.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishNet.Object;
+using FishNet.Demo.AdditiveScenes;
 
-public class FoodSpawner : MonoBehaviour
+//Made by Bobsi Unity - Youtube
+public class FoodSpawner : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject food_prefab;
+    public GameObject trash_prefab;
+    [HideInInspector] public List<GameObject> spawnedObject = new List<GameObject>();
+    List<GameObject> food_list = new List<GameObject>();
+    List<GameObject> trash_list = new List<GameObject>();
+
+
+    public override void OnStartClient()
     {
-        
+        base.OnStartClient();
+        if (!base.IsOwner)
+            GetComponent<FoodSpawner>().enabled = false;
+        SpawnFoodTrash();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+    }
+
+    public void SpawnFoodTrash()
+    {
+        int num_spawns;
+
+        num_spawns = transform.childCount;
+        foreach (Transform spawn_point in transform)
+        {
+            SpawnObject(Random.value > 0.5, spawn_point.position, spawn_point.rotation, this);
+        }
+    }
+
+    [ServerRpc]
+    public void SpawnObject(bool food_or_trash, Vector3 position, Quaternion rotation, FoodSpawner script)
+    {
+        GameObject spawned = Instantiate(food_or_trash ? food_prefab : trash_prefab, position, rotation);
+        ServerManager.Spawn(spawned);
+        SetSpawnedObject(spawned, script, food_or_trash);
+    }
+
+    [ObserversRpc]
+    public void SetSpawnedObject(GameObject spawned, FoodSpawner script, bool food_or_trash)
+    {
+        if (food_or_trash)
+        {
+            script.food_list.Add(spawned);
+        }
+        else
+        {
+            script.trash_list.Add(spawned);
+        }
+        script.spawnedObject.Add(spawned);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void DespawnObject(GameObject obj)
+    {
+        ServerManager.Despawn(obj);
     }
 }
