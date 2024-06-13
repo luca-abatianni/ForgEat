3a31fe6f1daa66d6cf6962d14459131f523726b2
luca-abatianni <s314883@studenti.polito.it>
2024-06-13 13:32:40 +0200

    Mergiato main
    Fixati errori, aggiunta provvisoria menu pausa

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -106,8 +106,6 @@ public class GameManager : NetworkBehaviour
                     return;
             }
         }
-
-        PauseGame();
     }
 
     void WaitForPlayers()
@@ -204,24 +202,6 @@ public class GameManager : NetworkBehaviour
             FoodPicker f_picker = player.GetComponent<FoodPicker>();
             f_picker.Client_FoodPickerSetEnabled(setting);
             f_picker.enabled = setting;
-        }
-    }
-
-    void PauseGame()
-    {
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            isPaused = !isPaused;
-            if(isPaused)
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
-            else 
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
         }
     }
 }
