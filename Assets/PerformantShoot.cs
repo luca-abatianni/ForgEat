2cea584d7c986c0a987d2877ae09921ae1c3b078
maciejlampart <maciejlampart@live.com>
2024-06-20 16:13:17 +0200

    Fixed sigil delay.
    Sigil and bullet are now spawned with separate RPC to maintain the intended delay between them

--- Assets/PerformantShoot.cs
+++ Assets/PerformantShoot.cs
@@ -12,6 +12,9 @@ public class PerformantShoot : NetworkBehaviour
     [SerializeField] public GameObject _sigilPoint;
     [SerializeField] public PowerBehavior.PowerType _primaryPower = PowerBehavior.PowerType.IceBullet;
 
+    [SerializeField] private float sigil_delay;
+    [SerializeField] private float fire_offset;
+
     [SerializeField]
     public List<GameObject> _listEffects = new List<GameObject>();
     [SerializeField]
@@ -21,7 +24,14 @@ public class PerformantShoot : NetworkBehaviour
     public GameObject _sigilToSpawn;
 
 
-    public void Shoot(float fire_offset)
+    public void Shoot()
+    {
+        //LocalSpawnSigil(_firePoint.transform, (int) _primaryPower);
+        SRPC_SpawnSigil(_firePoint.transform, (int)_primaryPower);
+        StartCoroutine(ShootingCoroutine());
+    }
+
+    private IEnumerator ShootingCoroutine()
     {
         Transform camera_transform = Camera.main.transform;
         Vector3 start_position = camera_transform.position + camera_transform.forward * fire_offset;
@@ -29,52 +39,67 @@ public class PerformantShoot : NetworkBehaviour
         Vector3 direction = camera_transform.forward;
 
         int bulletID = Random.Range(0, 999999);
-        SpawnLocal(start_position, rotation, _firePoint.transform, _primaryPower, bulletID, LocalConnection.ClientId);
-        SRPC_SpawnMagicBullet(start_position, rotation, direction, _firePoint.transform, _primaryPower, TimeManager.Tick, bulletID, OwnerId);
+
+        yield return new WaitForSeconds(sigil_delay);
+
+        SpawnLocal(start_position, rotation, _firePoint.transform, (int) _primaryPower, bulletID, LocalConnection.ClientId);
+        SRPC_SpawnMagicBullet(start_position, rotation, direction, _firePoint.transform, (int) _primaryPower, TimeManager.Tick, bulletID, OwnerId);
     }
 
-    private void SpawnLocal(Vector3 start_position, Quaternion rotation, Transform sigil_point, PowerBehavior.PowerType power, int bulletID, int ownerID)
+    private void SpawnLocal(Vector3 start_position, Quaternion rotation, Transform sigil_point, int power, int bulletID, int ownerID)
     {
+        // Spawn bullet
         Vector3 direction = rotation * Vector3.forward;
 
-        _effectToSpawn = _listEffects[(int) power];
+        _effectToSpawn = _listEffects[power];
         GameObject bullet = Instantiate(_effectToSpawn, start_position, rotation);
 
         bullet.GetComponent<Bullet>().Initialize(direction, bulletID, ownerID);
-        bullet.GetComponent<PowerBehavior>().SetPowerType(power);
+        bullet.GetComponent<PowerBehavior>().SetPowerType((PowerBehavior.PowerType) power);
 
-        _sigilToSpawn = _listSigils[(int) power];
-        SpawnSigil(_sigilToSpawn, _firePoint);
-        SpawnSigilFoot(_sigilToSpawn, _firePoint);
     }
 
 
     [ServerRpc(RequireOwnership = false)]
-    private void SRPC_SpawnMagicBullet(Vector3 start_position, Quaternion rotation, Vector3 direction, Transform sigil_point, PowerBehavior.PowerType power, uint start_tick, int bulletID, int ownerID)
+    private void SRPC_SpawnMagicBullet(Vector3 start_position, Quaternion rotation, Vector3 direction, Transform sigil_point, int power, uint start_tick, int bulletID, int ownerID)
     {
         ORPC_SpawnBulletObserver(start_position, rotation, direction, sigil_point, power, start_tick, bulletID, ownerID);
     }
 
     [ObserversRpc(ExcludeOwner = true)]
-    private void ORPC_SpawnBulletObserver(Vector3 start_position, Quaternion rotation, Vector3 direction, Transform sigil_point, PowerBehavior.PowerType power, uint start_tick, int bulletID, int ownerID)
+    private void ORPC_SpawnBulletObserver(Vector3 start_position, Quaternion rotation, Vector3 direction, Transform sigil_point, int power, uint start_tick, int bulletID, int ownerID)
     {
         float time_diff = (float)(TimeManager.Tick - start_tick) / TimeManager.TickRate;
         Vector3 spawn_position = start_position + direction * bullet_speed * time_diff;
         SpawnLocal(spawn_position, rotation, sigil_point, power, bulletID, ownerID);
     }
 
+    void LocalSpawnSigil(Transform _firePoint, int power)
+    {
+        _sigilToSpawn = _listSigils[power];
+        var spawned_sigil = Instantiate(_sigilToSpawn, _firePoint.transform.position, _firePoint.transform.rotation);
+        spawned_sigil.transform.SetParent(_firePoint.transform);
 
-    void SpawnSigil(GameObject _effectToSpawn, GameObject _firePoint)
-    {
-        var spawned = Instantiate(_effectToSpawn, _firePoint.transform.position, _firePoint.transform.rotation);
-        spawned.transform.SetParent(_firePoint.transform);
-    }
-    void SpawnSigilFoot(GameObject _effectToSpawn, GameObject _firePoint)
-    {
         var p = _firePoint.transform.position;
-        var spawned = Instantiate(_effectToSpawn, new Vector3(p.x, p.y, p.z), Quaternion.LookRotation(Vector3.up));
-        spawned.transform.SetParent(_firePoint.transform);
+        var spawned_sigil_foot = Instantiate(_sigilToSpawn, new Vector3(p.x, p.y, p.z), Quaternion.LookRotation(Vector3.up));
+        spawned_sigil_foot.transform.SetParent(_firePoint.transform);
     }
 
+    [ServerRpc(RequireOwnership = false)]
+    void SRPC_SpawnSigil(Transform _firePoint, int power)
+    {
+        ORPC_SpawnSigil(_firePoint, power);
+    }
 
+    [ObserversRpc]
+    void ORPC_SpawnSigil(Transform _firePoint, int power)
+    {
+        _sigilToSpawn = _listSigils[power];
+        var spawned_sigil = Instantiate(_sigilToSpawn, _firePoint.transform.position, _firePoint.transform.rotation);
+        spawned_sigil.transform.SetParent(_firePoint.transform);
+
+        var p = _firePoint.transform.position;
+        var spawned_sigil_foot = Instantiate(_sigilToSpawn, new Vector3(p.x, p.y, p.z), Quaternion.LookRotation(Vector3.up));
+        spawned_sigil_foot.transform.SetParent(_firePoint.transform);
+    }
 }
--- Assets/PrimaryPower.cs
+++ Assets/PrimaryPower.cs
@@ -35,8 +35,8 @@ public class PrimaryPower : NetworkBehaviour
         {
 
             _cooldown = Time.time + 1f;
-
-            StartCoroutine(SpawnMagic());
+            animator.SetBool("attackFreeze", true);
+            performant_shoot.Shoot();
         }
         else
         {
@@ -63,44 +63,4 @@ public class PrimaryPower : NetworkBehaviour
             performant_shoot._primaryPower = PowerBehavior.PowerType.TrickBullet;
         }
     }
-
-    private IEnumerator SpawnMagic()
-    {
-        animator.SetBool("attackFreeze", true);
-        //yield return new WaitForSeconds(.3f);
-        //SRPC_SpawnMagic(this, _effectToSpawn, _firePoint, gameObject, spawn_forward, spawn_rot);
-        GetComponent<PerformantShoot>().Shoot(0.5f);
-        yield return null;
-    }
-
-          
-    [ObserversRpc]
-    void ORPC_SetParent(GameObject parent, GameObject spawned)
-    {
-        spawned.transform.SetParent(parent.transform);
-    }
-    [ServerRpc]
-    void SRPC_SpawnMagic(PrimaryPower script, GameObject _effectToSpawn, GameObject _firePoint, GameObject player, Vector3 spawn_forward, Quaternion spawn_rot)
-    {
-        GameObject spawned;
-        if (_firePoint != null)
-        {
-            spawned = Instantiate(_effectToSpawn, _firePoint.transform.position, _firePoint.transform.rotation);
-            ServerManager.Spawn(spawned);
-            //GetComponent<PowerBehavior>().SetDirection(spawn_forward);
-            //spawned.GetComponent<PowerBehavior>().SetParent(_firePoint);
-            spawned.GetComponent<PowerBehavior>().SetSpawner(player);
-            //spawned.GetComponent<PowerBehavior>().SetPowerType(_primaryPower);
-            Physics.IgnoreCollision(player.GetComponent<Collider>(), spawned.GetComponent<Collider>(), true);
-        }
-        else
-        {
-            Debug.LogError("ERR PrimaryPower - Missing FirePoint");
-        }
-    }
-    //[ObserversRpc]
-    //public void SetSpawnedMagic(PrimaryPower script, GameObject spawned)
-    //{
-    //    script._effectToSpawn = spawned;
-    //}
 }
