ad1ca9fd99374c5e5d4313c4c6733650da12d37a
luca-abatianni <s314883@studenti.polito.it>
2024-07-07 12:03:35 +0200

    Test su menu

--- Assets/Scripts/CameraRotationMenu.cs
+++ Assets/Scripts/CameraRotationMenu.cs
@@ -17,4 +17,58 @@ public class CameraRotationMenu : MonoBehaviour
         transform.Rotate(new Vector3(0.0f, -7.0f, 0.0f));
         transform.position -= new Vector3 (0.0f, 0.0f, 80.0f);
     }
+
+    IEnumerator LerpRotateCamera()
+    {
+        float timeSinceStarted = 0f;
+        Vector3 newPosition = transform.position + new Vector3 (0.0f, 0.0f, 80.0f);
+        Quaternion newRotation = transform.rotation * Quaternion.Euler(0.0f, 7.0f, 0.0f);
+        
+        while (true)
+        {
+            timeSinceStarted += Time.deltaTime;
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, newPosition, timeSinceStarted), Quaternion.Lerp(transform.rotation, newRotation, timeSinceStarted));
+
+            // If the object has arrived, stop the coroutine
+            if (transform.position == newPosition)
+            {
+                yield break;
+            }
+
+            // Otherwise, continue next frame
+            yield return null;
+        }
+    }
+
+    public void LerpRotateCameraWrapper()
+    {
+        StartCoroutine(LerpRotateCamera());
+    }
+
+    IEnumerator LerpRotateBackCamera()
+    {
+        float timeSinceStarted = 0f;
+        Vector3 newPosition = transform.position - new Vector3 (0.0f, 0.0f, 80.0f);
+        Quaternion newRotation = transform.rotation * Quaternion.Euler(0.0f, -7.0f, 0.0f);
+        
+        while (true)
+        {
+            timeSinceStarted += Time.deltaTime;
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, newPosition, timeSinceStarted), Quaternion.Lerp(transform.rotation, newRotation, timeSinceStarted));
+
+            // If the object has arrived, stop the coroutine
+            if (transform.position == newPosition)
+            {
+                yield break;
+            }
+
+            // Otherwise, continue next frame
+            yield return null;
+        }
+    }
+
+    public void LerpRotateBackCameraWrapper()
+    {
+        StartCoroutine(LerpRotateBackCamera());
+    }
 }
