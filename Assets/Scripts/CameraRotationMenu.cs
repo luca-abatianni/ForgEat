c4745f48c807b005e90887d940151fef7a92b1dc
luca-abatianni <s314883@studenti.polito.it>
2024-07-08 12:24:07 +0200

    Menu dinamico ma scelte non funzionanti

--- Assets/Scripts/CameraRotationMenu.cs
+++ Assets/Scripts/CameraRotationMenu.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class CameraRotationMenu : MonoBehaviour
 {
-    public float speed = 1.0f;
+    private float speed = 1.0f;
+    private float cameraSpeed = 3.0f;
+    public Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
 
     public void RotateCamera()
     {
@@ -70,5 +77,62 @@ public class CameraRotationMenu : MonoBehaviour
     public void LerpRotateBackCameraWrapper()
     {
         StartCoroutine(LerpRotateBackCamera());
+    }
+
+    IEnumerator LerpRotateCameraGameSettings()
+    {
+        float timeSinceStarted = 0f;
+        Vector3 newPosition = transform.position + new Vector3 (52.2f, 2.7f, -130.6f);
+        Quaternion newRotation = transform.rotation * Quaternion.Euler(0.0f, 38.0f, 0.0f);
+        
+        while (true)
+        {
+            timeSinceStarted += Time.deltaTime;
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, newPosition, timeSinceStarted), Quaternion.Lerp(transform.rotation, newRotation, timeSinceStarted));
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 5.0f, Time.deltaTime * cameraSpeed);
+
+            // If the object has arrived, stop the coroutine
+            if (transform.position == newPosition)
+            {
+                yield break;
+            }
+
+            // Otherwise, continue next frame
+            // eddai
+            yield return null;
+        }
+    }
+
+    public void LerpRotateCameraGameSettingsWrapper()
+    {
+        StartCoroutine(LerpRotateCameraGameSettings());
+    }
+
+    IEnumerator LerpRotateBackCameraGameSettings()
+    {
+        float timeSinceStarted = 0f;
+        Vector3 newPosition = transform.position + new Vector3 (-52.2f, -2.7f, 130.6f);
+        Quaternion newRotation = transform.rotation * Quaternion.Euler(0.0f, -38.0f, 0.0f);
+        
+        while (true)
+        {
+            timeSinceStarted += Time.deltaTime;
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, newPosition, timeSinceStarted), Quaternion.Lerp(transform.rotation, newRotation, timeSinceStarted));
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, 9.0f, Time.deltaTime * cameraSpeed);
+
+            // If the object has arrived, stop the coroutine
+            if (transform.position == newPosition)
+            {
+                yield break;
+            }
+
+            // Otherwise, continue next frame
+            yield return null;
+        }
+    }
+
+    public void LerpRotateBackCameraGameSettingsWrapper()
+    {
+        StartCoroutine(LerpRotateBackCameraGameSettings());
     }
 }
