9e4a3690f2ec522ec1109d80b1db80373d3d545e
luca-abatianni <s314883@studenti.polito.it>
2024-07-17 19:50:47 +0200

    fixed spawn points

--- Assets/Scripts/CharacterSpawn.cs
+++ Assets/Scripts/CharacterSpawn.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class CharacterSpawn : NetworkBehaviour
 {
     public List<GameObject> playerSkins = new List<GameObject>();
-    public GameObject spawnObject;
 
     public override void OnStartClient()
     {
@@ -18,12 +17,7 @@ public class CharacterSpawn : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     public void Spawn(int skinIndex, NetworkConnection conn)
     {
-        Debug.Log("!!!!!-----Spawn Called");
-        List<GameObject> spawnPoints = new List<GameObject>();
-        for (int i=0; i < spawnObject.transform.childCount; i++)
-            spawnPoints.Add(spawnObject.transform.GetChild(i).gameObject);
-            
-        GameObject player = Instantiate(playerSkins[skinIndex], spawnPoints[0].transform.position, Quaternion.identity);
+        GameObject player = Instantiate(playerSkins[skinIndex], this.transform.position, Quaternion.identity);
         Spawn(player, conn);
     }
 }
