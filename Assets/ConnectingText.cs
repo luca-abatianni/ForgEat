b0685087cdc9a0862eb9898bb2d45fba5417959a
luca-abatianni <s314883@studenti.polito.it>
2024-07-18 20:36:01 +0200

    connecting... text added

--- Assets/ConnectingText.cs
+++ Assets/ConnectingText.cs
@@ -8,6 +8,7 @@ public class ConnectingText : MonoBehaviour
 {
     TMP_Text text;
     int i = 0;
+    int j = 0;
     string[] items = { "Connecting", "Connecting.", "Connecting..", "Connecting..." };
     void Start() 
     {
@@ -25,6 +26,13 @@ public class ConnectingText : MonoBehaviour
                 i=0;
 
             await Task.Delay(200);
+            j++;
+            if (j==20)
+                break;
         }
+
+        text.SetText("");
+        gameObject.SetActive(false);
+        
     }
 }
